607f354802c60811cada65be4833eb951c02a27f
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-08-08 23:28:30 -0400

    Db set up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marben', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Origin', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McNip', true);
+INSERT INTO burgers (burger_name) VALUES ('Smashburger');
