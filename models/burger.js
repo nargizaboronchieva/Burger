06d903714f6c330e6644fbe8afa8b3191056aa1d
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-08-12 03:03:00 -0400

    setting up front end JS and jQuery

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,24 @@
-// Import the ORM to create functions that will interact with the database.
+// Import orm.js to create functions that will interact with
+// the database.
 var orm = require("../config/orm.js");
 
-var cat = {
-  all: function(cb) {
-    orm.all("cats", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
-      cb(res);
-    });
-  }
+var burger = {
+	selectAll: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(valOfCol, valOfOtherCol, cb) {
+		orm.insertOne("burgers", valOfCol, valOfOtherCol, function(res) {
+			cb(res);
+		});
+	},
+	updateOne: function(columnInput, condition, cb) {
+		orm.updateOne("burgers", columnInput, condition, function(res) {
+			cb(res);
+		});
+	}
 };
 
-// Export the database functions for the controller (catsController.js).
-module.exports = cat;
+// Export database functions for the controller (burgers_controller.js)
+module.exports = burger;
